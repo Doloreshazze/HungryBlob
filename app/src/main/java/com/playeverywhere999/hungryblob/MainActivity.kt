924b4ef85f7933b81b7e995d86d3883b654064dd
Doloreshazze <daw81bk@gmail.com>
2026-05-20 15:52:50 +0200

    Fix unresolved asPaddingValues import for HUD insets

--- app/src/main/java/com/playeverywhere999/hungryblob/MainActivity.kt
+++ app/src/main/java/com/playeverywhere999/hungryblob/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
